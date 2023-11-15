601da44d992ed4f325f94eb8dfb4722c7bb87059
Julien <berge.jul@gmail.com>
2023-11-15 11:07:09 +0100

    Feat() : User Storie #8 - Complétion objectif

--- src/pages/UserInfo/index.tsx
+++ src/pages/UserInfo/index.tsx
@@ -9,21 +9,25 @@ function UserInfo() {
   const id = Number(stringId) || 0
   console.log(id)
 
-  const {
-    data: mainData,
-  } = useFetch('http://localhost:3000/user/${userId}', id)
+  const { data: mainData } = useFetch(
+    'http://localhost:3000/user/${userId}',
+    id
+  )
 
-  const {
-    data: activityData,
-  } = useFetch('http://localhost:3000/user/${userId}/activity', id)
+  const { data: activityData } = useFetch(
+    'http://localhost:3000/user/${userId}/activity',
+    id
+  )
 
-  const {
-    data: sessionsData,
-  } = useFetch('http://localhost:3000/user/${userId}/average-sessions', id)
+  const { data: sessionsData } = useFetch(
+    'http://localhost:3000/user/${userId}/average-sessions',
+    id
+  )
 
-  const {
-    data: performanceData,
-  } = useFetch('http://localhost:3000/user/${userId}/performance', id)
+  const { data: performanceData } = useFetch(
+    'http://localhost:3000/user/${userId}/performance',
+    id
+  )
 
   useEffect(() => {
     if (mainData && activityData && sessionsData && performanceData) {
@@ -38,11 +42,14 @@ function UserInfo() {
   }, [mainData, activityData, sessionsData, performanceData])
 
   return (
-    <div className='  m-11'>
+    <div className="  m-11">
+      <h2 className=' text-2xl'>Infos utilisateur :</h2><br />
       <p>Id : {data?.getUserInfo().id}</p>
       <p>firstName : {data?.getUserInfo().firstName}</p>
       <p>lastName : {data?.getUserInfo().lastName}</p>
-      <p>age : {data?.getUserInfo().age}</p>
+      <p>age : {data?.getUserInfo().age}</p><br />
+      <h2 className=' text-2xl'>Complétion objectif :</h2><br />
+      <p>complétion de l’objectif journalier : {data?.getScore()[0]?.value}</p><br />
     </div>
   )
 }
